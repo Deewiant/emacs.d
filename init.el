d40afa89a12e5fc951444327c3583e1d85086ddd
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2015-01-31 21:42:17 +0200

    Start idle company-mode completion sooner

--- init.el
+++ init.el
@@ -39,6 +39,8 @@
 (setq-default c-default-style "deewiant")
 
 (add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 2)
 
 ; Backup into a global directory instead of next to the edited file,
 ; and don't clobber symlinks.
