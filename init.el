849b59b6fadbea9b193d3510cf59ecc1c0849130
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2015-06-04 11:17:47 +0300

    Move single-use irony-mode hook into a lambda

--- init.el
+++ init.el
@@ -126,12 +126,12 @@
 (add-hook 'c-mode-hook 'my-irony-mode-enable)
 (add-hook 'c++-mode-hook 'my-irony-mode-enable)
 (add-hook 'objc-mode-hook 'my-irony-mode-enable)
-(defun my-irony-mode-hook ()
-   (define-key irony-mode-map [remap completion-at-point]
-      'irony-completion-at-point-async)
-   (define-key irony-mode-map [remap complete-symbol]
-      'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook
+          (lambda ()
+            (define-key irony-mode-map [remap completion-at-point]
+              'irony-completion-at-point-async)
+            (define-key irony-mode-map [remap complete-symbol]
+              'irony-completion-at-point-async)))
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
 (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
